e7f3a277f7e9464cec05d4da242e5040c4079ad5
srikanth-nuthalapati <srikanthnuthalapati@outlook.com>
2025-01-27 16:01:07 +0530

    3rd commit

--- config/dao.js
+++ config/dao.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-let db = mysql.createConnection({
+let db = mysql.createPool({
     host: process.env.host,
     port:process.env.db_port,
     user: process.env.user,
     password: process.env.password,
     database: process.env.database,
+    waitForConnections:true,
+    connectionLimit:15,
+    queueLimit:0
 });
 
 db.connect((err) => {
