9275f0cb53bc6f71addf1acd30b54c82331132f1
srikanth-nuthalapati <srikanthnuthalapati@outlook.com>
2025-01-29 08:53:32 +0530

    changed cors origin

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://go-cabs-fe-git-main-srikanth-ns-projects.vercel.app',
     credentials: true,
 }));
 
